ebdd9528955ff42eed9b3c960d9d6b881f7fa14c
jo3.pol@gmail.com <jo3.pol@gmail.com@166a7908-ef21-33b9-37c4-0ad16434d07c>
2013-01-16 16:09:29 +0000

    More efficient an easier construction of migration kml

--- src/test/java/gedcom2sem/BatchExamples.java
+++ src/test/java/gedcom2sem/BatchExamples.java
@@ -1,11 +1,24 @@
+// @formatter:off
+/*
+ * Copyright 2013, J. Pol
+ *
+ * This file is part of free software: you can redistribute it and/or modify it under the terms of the
+ * GNU General Public License as published by the Free Software Foundation.
+ *
+ * This package is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even
+ * the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * See the GNU General Public License for more details. A copy of the GNU General Public License is
+ * available at <http://www.gnu.org/licenses/>.
+ */
+// @formatter:on
 package gedcom2sem;
 
 import gedcom2sem.gedsem.Convert;
 import gedcom2sem.gedsem.UriFormats;
 import gedcom2sem.semweb.Mashup;
+import gedcom2sem.semweb.GedKml;
 import gedcom2sem.semweb.Select;
-
-import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.PrintStream;
@@ -15,37 +28,38 @@ import org.junit.Test;
 
 public class BatchExamples
 {
-    private static final String QUICK_RULES = new File("src/main/resources/rules/QuickRules.txt").getAbsolutePath();
-    private static final String MASHUP_ARQ = new File("src/main/resources/reports/mashup/mashup.arq").getAbsolutePath();
-    private static final String MIGRATIONS_ARQ = new File("src/main/resources/reports/mashup/MigrationLines.arq").getAbsolutePath();
-    private static final String GEDCOM = new File("src/test/resources/kennedy.ged").getAbsolutePath();
-    private static final String GEDCOM_TTL = new File("target/kennedy.ttl").getAbsolutePath();
-    private static final String MASHUP_TSV = new File("target/mashup.tsv").getAbsolutePath();
-    private static final String MASHUP_TTL = new File("target/mashup.ttl").getAbsolutePath();
-    private static final String REPORT_TXT = new File("target/report.txt").getAbsolutePath();
+    private static final String TEST = "src/test/resources/";
+    private static final String MAIN = "src/main/resources/";
+    private static final String QUERY_DIR = "src/main/resources/reports/mashup/";
+    private static final String RULES = "src/main/resources/rules/SlowRules.txt";
+    private PrintStream savedOut;
 
     @Ignore // Mashup takes too long to run on a regular basis
     @Test
     public void kennedy() throws Exception
     {
-        //TODO create MASHUP_TTL???
-        final PrintStream saved = redirectOutput(GEDCOM_TTL);
-        Convert.main("-rules", QUICK_RULES, "-uri", UriFormats.DEFAULT_URI, "-gedcom", GEDCOM);
-        restoreOutput(saved);
-        Select.main(GEDCOM_TTL, MASHUP_TSV, MASHUP_ARQ);
-        Mashup.main(GEDCOM_TTL, MASHUP_TSV, MASHUP_TTL, UriFormats.DEFAULT_URI, "de|fr");
-        Select.main(GEDCOM_TTL, MASHUP_TTL, REPORT_TXT, MIGRATIONS_ARQ);
+        redirectOut("target/kennedy.ttl");
+        Convert.main("-rules", RULES, "-uri", UriFormats.DEFAULT_URI, "-gedcom", TEST + "kennedy.ged");
+        restoreOut();
+
+        Select.main("target/kennedy.ttl", "target/mashup.tsv", QUERY_DIR + "mashup.arq");
+
+        // consider MASHUP_TSV changed manually into KENNEDY_TSV (geo name IDs added)
+        Mashup.main("target/kennedy.ttl", TEST + "kennedy.tsv", "target/mashup.ttl", UriFormats.DEFAULT_URI, "de|fr");
+
+        Select.main("target/kennedy.ttl", "target/mashup.ttl", "target/places.tsv", QUERY_DIR + "places.arq");
+     
+        GedKml.main(MAIN+"kml.properties","target/kennedy.ttl", "target/mashup.ttl", "target/places.kml", QUERY_DIR + "places.arq");
     }
 
-    private PrintStream redirectOutput(final String gedcomTtl) throws FileNotFoundException
+    private void redirectOut(final String filename) throws FileNotFoundException
     {
-        final PrintStream saved = System.out;
-        System.setOut(new PrintStream(new FileOutputStream(gedcomTtl)));
-        return saved;
+        savedOut = System.out;
+        System.setOut(new PrintStream(new FileOutputStream(filename)));
     }
-    
-    private void restoreOutput(final PrintStream saved)
+
+    private void restoreOut()
     {
-        System.setOut(saved);
+        System.setOut(savedOut);
     }
 }
