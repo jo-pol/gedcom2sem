1555e4520dcd285f90c4c4463b12c2affc8ce489
jo3.pol@gmail.com <jo3.pol@gmail.com@166a7908-ef21-33b9-37c4-0ad16434d07c>
2013-01-19 14:25:41 +0000

    fixed some KML/Mashup issues; added copyright headers (commit phase 6)

--- src/test/java/gedcom2sem/semweb/QueryWithPluginInterfaceTester.java
+++ src/test/java/gedcom2sem/semweb/QueryWithPluginInterfaceTester.java
@@ -1,3 +1,17 @@
+// @formatter:off
+/*
+ * Copyright 2013, J. Pol
+ *
+ * This file is part of free software: you can redistribute it and/or modify it under the terms of the
+ * GNU General Public License as published by the Free Software Foundation.
+ *
+ * This package is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even
+ * the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * See the GNU General Public License for more details. A copy of the GNU General Public License is
+ * available at <http://www.gnu.org/licenses/>.
+ */
+// @formatter:on
 package gedcom2sem.semweb;
 
 import gedcom2sem.gedsem.Parser;
@@ -21,10 +35,11 @@ import com.hp.hpl.jena.query.ResultSetFormatter;
 import com.hp.hpl.jena.query.Syntax;
 import com.hp.hpl.jena.rdf.model.Model;
 
-public class QueryWithPluginInterface extends AstractQueryTest
+public class QueryWithPluginInterfaceTester extends AstractQueryTest
+// name does neither start nor end with test so maven can build a jar if web-resources don't cooperate
 {
 
-    public QueryWithPluginInterface(final Boolean mashup, final Integer expectedNrOfLines, final String queryFileName)
+    public QueryWithPluginInterfaceTester(final Boolean mashup, final Integer expectedNrOfLines, final String queryFileName)
     {
         super(mashup, expectedNrOfLines, queryFileName);
     }
