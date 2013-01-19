7515ef8bf9f9802b7d171dc171fea6495d19a278
jo3.pol@gmail.com <jo3.pol@gmail.com@166a7908-ef21-33b9-37c4-0ad16434d07c>
2013-01-19 14:25:09 +0000

    fixed some KML/Mashup issues; added copyright headers (commit phase 5)

--- trunk/src/test/java/gedcom2sem/BatchExamples.java
+++ trunk/src/test/java/gedcom2sem/BatchExamples.java
@@ -1,11 +1,15 @@
 // @formatter:off
 /*
- * Copyright 2013, J. Pol This file is part of free software: you can redistribute it and/or modify it
- * under the terms of the GNU General Public License as published by the Free Software Foundation. This
- * package is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the
- * implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public
- * License for more details. A copy of the GNU General Public License is available at
- * <http://www.gnu.org/licenses/>.
+ * Copyright 2013, J. Pol
+ *
+ * This file is part of free software: you can redistribute it and/or modify it under the terms of the
+ * GNU General Public License as published by the Free Software Foundation.
+ *
+ * This package is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even
+ * the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * See the GNU General Public License for more details. A copy of the GNU General Public License is
+ * available at <http://www.gnu.org/licenses/>.
  */
 // @formatter:on
 package gedcom2sem;
@@ -24,11 +28,11 @@ import org.junit.Test;
 
 public class BatchExamples
 {
-    private static final String TEST      = "src/test/resources/";
-    private static final String MAIN      = "src/main/resources/";
+    private static final String TEST = "src/test/resources/";
+    private static final String MAIN = "src/main/resources/";
     private static final String QUERY_DIR = "src/main/resources/reports/mashup/";
-    private static final String RULES     = "src/main/resources/rules/SlowRules.txt";
-    private PrintStream         savedOut;
+    private static final String RULES = "src/main/resources/rules/SlowRules.txt";
+    private PrintStream savedOut;
 
     @Ignore
     // Mashup takes too long to run on a regular basis
@@ -42,7 +46,7 @@ public class BatchExamples
         Select.main("target/kennedy.ttl", "target/mashup.tsv", QUERY_DIR + "mashup.arq");
 
         // consider MASHUP_TSV changed manually into KENNEDY_TSV (geo name IDs added)
-        Mashup.main("target/kennedy.ttl", TEST + "kennedy.tsv", "target/mashup.ttl", UriFormats.DEFAULT_URI, "de|fr");
+        Mashup.main(TEST + "kennedy.tsv", "http://my.domain.com/places#", "target/mashup.ttl", "de|fr");
 
         Select.main("target/kennedy.ttl", "target/mashup.ttl", "target/places.tsv", QUERY_DIR + "classmates.arq");
 
