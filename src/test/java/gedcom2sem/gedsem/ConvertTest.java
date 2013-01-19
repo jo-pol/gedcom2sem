6f8c5f6b0d0502b918d9d52c06817f9fd7cff2e8
jo3.pol@gmail.com <jo3.pol@gmail.com@166a7908-ef21-33b9-37c4-0ad16434d07c>
2013-01-19 14:25:09 +0000

    fixed some KML/Mashup issues; added copyright headers (commit phase 5)

--- src/test/java/gedcom2sem/gedsem/ConvertTest.java
+++ src/test/java/gedcom2sem/gedsem/ConvertTest.java
@@ -1,3 +1,17 @@
+// @formatter:off
+/*
+ * Copyright 2013, J. Pol
+ *
+ * This file is part of free software: you can redistribute it and/or modify it under the terms of the
+ * GNU General Public License as published by the Free Software Foundation.
+ *
+ * This package is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even
+ * the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * See the GNU General Public License for more details. A copy of the GNU General Public License is
+ * available at <http://www.gnu.org/licenses/>.
+ */
+// @formatter:on
 package gedcom2sem.gedsem;
 
 import org.junit.Test;
@@ -8,7 +22,7 @@ public class ConvertTest
     @Test(expected = IllegalArgumentException.class)
     public void noArgs() throws Exception
     {
-        Convert.main((String[])null);
+        Convert.main((String[]) null);
     }
 
     @Test(expected = IllegalArgumentException.class)
