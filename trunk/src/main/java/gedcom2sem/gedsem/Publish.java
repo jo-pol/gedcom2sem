2cd8fce268384ab965e0bdde27c0e4b4e0368345
jo3.pol@gmail.com <jo3.pol@gmail.com@166a7908-ef21-33b9-37c4-0ad16434d07c>
2013-03-12 13:20:59 +0000

    fixed the filter for original statements

--- trunk/src/main/java/gedcom2sem/gedsem/Publish.java
+++ trunk/src/main/java/gedcom2sem/gedsem/Publish.java
@@ -3,12 +3,11 @@ package gedcom2sem.gedsem;
 import gedcom2sem.io.FileUtil;
 
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.PrintStream;
+import java.util.Set;
 
 import com.hp.hpl.jena.rdf.model.Model;
 import com.hp.hpl.jena.rdf.model.ModelFactory;
-import com.hp.hpl.jena.rdf.model.Resource;
 import com.hp.hpl.jena.rdf.model.Statement;
 
 public class Publish
@@ -16,39 +15,25 @@ public class Publish
     public static void main(final String... files) throws Exception
     {
         // TODO read geoMashup, but then file types don't tell input from output
-        Convert.main(files);
         // TODO connect each FAM/INDI with provenance in HEAD
-        // TODO redesign cleanup strategy:
-        // deleting original statements deleted inferred statements
-        // rather copy inferred statements to new model
+        final Set<Statement> inferredStatements = Convert.execute(files);
+        final Model modelOut = ModelFactory.createDefaultModel();
+        for (final Statement stmt : inferredStatements)
+            modelOut.add(stmt.getSubject(),stmt.getPredicate(),stmt.getObject());
+        // TODO add name space prefixes, must be user specified along with the rules
         for (final String file : files)
         {
-            final String language;
             try
             {
-                language = FileUtil.guessLanguage(new File(file));
+                final String language = FileUtil.guessLanguage(new File(file));
+                modelOut.write(new PrintStream(file), language);
+                System.err.println("written: "+modelOut.listStatements().toList().size());
+                return;
             }
             catch (final IllegalArgumentException e)
             {
                 continue;
             }
-            final Model model = ModelFactory.createDefaultModel();
-            model.read(new FileInputStream(file), (String) null, language);
-            System.err.println("" + model.size());
-            for (final Statement stmt : model.listStatements().toList())
-                if (stmt.getPredicate().toString().startsWith("http://genj"))
-                    model.remove(stmt);
-            System.err.println("" + model.size());
-            for (final Statement stmt : model.listStatements().toList())
-            {
-                final Resource subject = stmt.getSubject();
-                if (subject.toString().startsWith("-"))
-                    // if(! model.listObjectsOfProperty(new Property(subject)).hasNext())
-                    model.remove(stmt);
-            }
-            System.err.println("" + model.size());
-            final PrintStream output = new PrintStream(file);
-            model.write(output, language);
         }
     }
 }
