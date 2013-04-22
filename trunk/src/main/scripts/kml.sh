06e417a511e31ed9edf291377c5f534ba8cfce4a
jo3.pol@gmail.com <jo3.pol@gmail.com@166a7908-ef21-33b9-37c4-0ad16434d07c>
2013-04-22 07:04:03 +0000

    added command line example for query from INDI-context

--- trunk/src/main/scripts/kml.sh
+++ trunk/src/main/scripts/kml.sh
@@ -1,1 +1,1 @@
-java -Xmx1024M -cp gedcom2sem-0.2.3-SNAPSHOT.jar gedcom2sem.semweb.KmlGenerator conf/kml-by-birth.properties conf/reports/places-by-birth.arq kennedy.ttl kennedy.kml
+java -Xmx1024M -cp ${project.build.finalName}.jar gedcom2sem.semweb.KmlGenerator conf/kml-by-birth.properties conf/reports/places-by-birth.arq kennedy.ttl kennedy.kml
